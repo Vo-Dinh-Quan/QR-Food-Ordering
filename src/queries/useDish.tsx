a466a5f60ee64b2dbde0774852afbdddebe5afd9
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2025-03-04 01:44:45 +0700

    feat(api): declare API endpoints for food management

---
+++ src/queries/useDish.tsx
@@ -0,0 +1,66 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  AccountResType,
+  UpdateEmployeeAccountBodyType,
+} from "@/schemaValidations/account.schema";
+import dishApiRequest from "@/apiRequests/dish";
+import { UpdateDishBodyType } from "@/schemaValidations/dish.schema";
+
+export const useDishListQuery = () => {
+  return useQuery({
+    queryKey: ["dishes"],
+    queryFn: dishApiRequest.list,
+  });
+};
+
+export const useGetDishQuery = ({ id, enable }: { id: number, enable: boolean }) => {
+  return useQuery({
+    queryKey: ["dishes", id],
+    queryFn: () => dishApiRequest.getDish(id),
+    enabled: enable
+  });
+};
+
+export const useAddDishMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: dishApiRequest.add,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["dishes"],
+      });
+    },
+  });
+};
+
+export const useUpdateDishMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    // nhận vào 1 đối tượng có kiểu dữ liệu là UpdateEmployeeAccountBodyType và có thêm id
+    // cú pháp {id,...body} để lấy ra id và còn lại là body
+    mutationFn: ({id,...body}: UpdateDishBodyType & { id: number }) =>
+      dishApiRequest.updateDish(id, body),
+    onSuccess: (_, { id }) => {
+      // _ là kết quả của mutationFn, không sử dụng trong trường hợp này
+      // { id } là đối tượng chứa các tham số mà bạn đã truyền vào mutationFn
+      queryClient.invalidateQueries({
+        queryKey: ["dishes"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["dishes", id],
+      });
+    },
+  });
+};
+
+export const useDeleteDishMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn:  dishApiRequest.deleteDish,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["dishes"],
+      });
+    },
+  });
+}
