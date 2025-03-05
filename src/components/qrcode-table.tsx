04abe73cba2776847b06648101c488ce6d9f04c2
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2025-03-05 13:52:03 +0700

    feat(table): customize QR code information in canvas card

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef } from "react";
 export default function QRCodeTable({
   token,
   tableNumber,
-  width = 250,
+  width = 200,
 }: {
   token: string;
   tableNumber: number;
@@ -15,20 +15,46 @@ export default function QRCodeTable({
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      QRCode.toCanvas(
-        canvas,
-        getTableLink({
-          token: token, 
-          tableNumber: tableNumber,
-        }),
-        (error) => {
-          if (error) console.error(error);
-        }
-      );
-    }
-  }, [token, tableNumber]);
+    // hiện tại thư viện QR Code sẽ vẽ lên thẻ Canvas
+    // Bây giờ: Chúng ta sẽ tạo 1 cái thẻ canvas ảo để thư viện QRCode nó vẽ QR lên trên đó. Và chúng ta sẽ edit thẻ canvas thật.
+    // Cuối cùng thì chúng ta sẽ đưa cái thẻ canvas ảo chứa QRCode ở trên vào thẻ canvas thật.
+    const canvas = canvasRef.current!;
+    canvas.height = width + 40;
+    canvas.width = width;
+    const canvasContext = canvas.getContext("2d")!; // 2d là kiểu vẽ 2 chiều
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height); // tô màu trắng cho canvas từ tọa độ (0,0) đến (canvas.width, canvas.height)
+    canvasContext.font = "bold 15px Inter";
 
-  return <canvas ref={canvasRef} />;
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 15
+    );
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 30
+    );
+    const virtualCanvas = document.createElement("canvas");
+
+    QRCode.toCanvas(
+      virtualCanvas,
+      getTableLink({
+        token: token,
+        tableNumber: tableNumber,
+      }),{
+        width: width,
+        margin: 2,
+      },
+      function (error) {
+        if (error) console.error(error);
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width);
+      }
+    );
+  }, [token, tableNumber, width]);
+
+  return <canvas ref={canvasRef} className="font-semibold"/>;
 }
