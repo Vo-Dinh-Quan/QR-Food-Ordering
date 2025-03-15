a54dbec064bdf3dba4020f363921bac3a7e98331
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2025-03-15 20:02:16 +0700

    feat(ui): build food menu UI

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         pathname: '/**',
         search: '',
       },
+      {
+        hostname: 'unsplash.com',
+        pathname: '/**',
+      },
     ],
   },
 };
