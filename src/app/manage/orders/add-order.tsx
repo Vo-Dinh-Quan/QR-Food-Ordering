aa3eab7cd48de182c133478becc8808161fee696
Vo Dinh Quan <22521193@gm.uit.edu.vn>
2025-03-24 12:36:53 +0700

    feat(order): implement logic for creating orders for new and existing customers

--- src/app/manage/orders/add-order.tsx
+++ src/app/manage/orders/add-order.tsx
@@ -8,7 +8,7 @@ import {
 	DialogTrigger,
 } from "@/components/ui/dialog";
 import { PlusCircle } from "lucide-react";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -26,9 +26,13 @@ import GuestsDialog from "@/app/manage/orders/guests-dialog";
 import { CreateOrdersBodyType } from "@/schemaValidations/order.schema";
 import Quantity from "@/app/guest/menu/quantity";
 import Image from "next/image";
-import { cn, formatCurrency } from "@/lib/utils";
+import { cn, formatCurrency, handleErrorApi } from "@/lib/utils";
 import { DishStatus } from "@/constants/type";
 import { DishListResType } from "@/schemaValidations/dish.schema";
+import { useDishListQuery } from "@/queries/useDish";
+import { useCreateOrderMutation } from "@/queries/useOrder";
+import { useCreateGuestMutation } from "@/queries/useAccount";
+import { toast } from "sonner";
 
 export default function AddOrder() {
 	const [open, setOpen] = useState(false);
@@ -37,15 +41,18 @@ export default function AddOrder() {
 	>(null);
 	const [isNewGuest, setIsNewGuest] = useState(true);
 	const [orders, setOrders] = useState<CreateOrdersBodyType["orders"]>([]);
-	const dishes: DishListResType["data"] = [];
+	const { data } = useDishListQuery();
+	const dishes = data?.payload.data ?? [];
 
-	const totalPrice = useMemo(() => {
+	const totalPrice = () => {
 		return dishes.reduce((result, dish) => {
 			const order = orders.find((order) => order.dishId === dish.id);
 			if (!order) return result;
 			return result + order.quantity * dish.price;
 		}, 0);
-	}, [dishes, orders]);
+	};
+	const createOrderMutation = useCreateOrderMutation();
+	const createGuestMutation = useCreateGuestMutation();
 
 	const form = useForm<GuestLoginBodyType>({
 		resolver: zodResolver(GuestLoginBody),
@@ -67,15 +74,73 @@ export default function AddOrder() {
 				return [...prevOrders, { dishId, quantity }];
 			}
 			const newOrders = [...prevOrders];
-			newOrders[index] = { ...newOrders[index], quantity };
+			newOrders[index] = { ...newOrders[index], quantity }; // cú pháp này có nghĩa là ghi đè quantity của phần tử tại vị trí index
 			return newOrders;
 		});
 	};
 
-	const handleOrder = async () => {};
+	const handleOrder = async () => {
+		try {
+			let guestId = selectedGuest?.id;
+			console.log("guestId", guestId);
+			if (isNewGuest) {
+				if (!name || !tableNumber) {
+					toast("Thông tin không đầy đủ", {
+						description: "Vui lòng nhập tên khách hàng và chọn bàn",
+						action: {
+							label: "Ẩn",
+							onClick: () => console.log("Undo"),
+						},
+					});
+					return;
+				}
+				const guestRes = await createGuestMutation.mutateAsync({
+					name,
+					tableNumber,
+				});
+				guestId = guestRes.payload.data.id;
+			}
+			if (!guestId) {
+				toast("Không thể đặt món", {
+					description: "Vui lòng chọn khách hàng",
+					action: {
+						label: "Ẩn",
+						onClick: () => console.log("Undo"),
+					},
+				});
+				return;
+			}
+			await createOrderMutation.mutateAsync({
+				guestId,
+				orders,
+			});
+			reset();
+		} catch (error) {
+			handleErrorApi({
+				error,
+				setError: form.setError,
+			});
+			reset();
+		}
+	};
+
+	const reset = () => {
+		form.reset();
+		setSelectedGuest(null);
+		setIsNewGuest(true);
+		setOrders([]);
+		setOpen(false);
+	};
 
 	return (
-		<Dialog onOpenChange={setOpen} open={open}>
+		<Dialog
+			onOpenChange={(value) => {
+				if (!value) {
+					reset();
+				}
+				setOpen(value);
+			}}
+			open={open}>
 			<DialogTrigger asChild>
 				<Button size="sm" className="h-7 gap-1">
 					<PlusCircle className="h-3.5 w-3.5" />
@@ -124,6 +189,7 @@ export default function AddOrder() {
 									control={form.control}
 									name="tableNumber"
 									render={({ field }) => (
+										// Cơ chế hoạt động thao tác của FormItem này là khi field.onChange được gọi, thì giá trị của field.value sẽ thay đổi
 										<FormItem>
 											<div className="grid grid-cols-4 items-center justify-items-start gap-4">
 												<Label htmlFor="tableNumber">Chọn bàn</Label>
@@ -168,12 +234,13 @@ export default function AddOrder() {
 					.map((dish) => (
 						<div
 							key={dish.id}
-							className={cn("flex gap-4", {
-								"pointer-events-none": dish.status === DishStatus.Unavailable,
+							className={cn("flex gap-4 items-center", {
+								"pointer-events-none opacity-50":
+									dish.status === DishStatus.Unavailable,
 							})}>
 							<div className="flex-shrink-0 relative">
 								{dish.status === DishStatus.Unavailable && (
-									<span className="absolute inset-0 flex items-center justify-center text-sm">
+									<span className="absolute inset-0 flex text-center items-center justify-center bg-black/50 text-white text-xs font-bold rounded-md">
 										Hết hàng
 									</span>
 								)}
@@ -183,12 +250,11 @@ export default function AddOrder() {
 									height={100}
 									width={100}
 									quality={100}
-									className="object-cover w-[80px] h-[80px] rounded-md"
+									className="object-cover w-[40px] h-[40px] rounded-md"
 								/>
 							</div>
 							<div className="space-y-1">
 								<h3 className="text-sm">{dish.name}</h3>
-								<p className="text-xs">{dish.description}</p>
 								<p className="text-xs font-semibold">
 									{formatCurrency(dish.price)}
 								</p>
@@ -210,7 +276,7 @@ export default function AddOrder() {
 						onClick={handleOrder}
 						disabled={orders.length === 0}>
 						<span>Đặt hàng · {orders.length} món</span>
-						<span>{formatCurrency(totalPrice)}</span>
+						<span>{formatCurrency(totalPrice())}</span>
 					</Button>
 				</DialogFooter>
 			</DialogContent>
